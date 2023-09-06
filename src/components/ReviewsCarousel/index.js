723eb3e7918d7697409b727a7689bfb9bed190e5
root <venky007sure@gmail.com>
2023-09-06 10:53:00 +0000

    review app

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,64 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initial: 0,
+    }
+  }
+
+  rightArrow = () => {
+    const {reviewsList} = this.props
+    this.setState(pre => ({
+      initial:
+        pre.initial === reviewsList.length - 1
+          ? reviewsList.length - 1
+          : pre.initial + 1,
+    }))
+  }
+
+  leftArrow = () => {
+    this.setState(pre => ({initial: pre.initial === 0 ? 0 : pre.initial - 1}))
+  }
+
+  render() {
+    const {initial} = this.state
+    const {reviewsList} = this.props
+    const {imgUrl, companyName, username, description} = reviewsList[initial]
+    return (
+      <div className="review">
+        <div className="review-block">
+          <h1 className="head">Reviews</h1>
+          <div className="arrow-con">
+            <button className="but" data-testid="leftArrow" type="button">
+              <img
+                alt="left arrow"
+                src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+                onClick={this.leftArrow}
+              />
+            </button>
+            <div className="img-user-con">
+              <img className="img-con" alt={username} src={imgUrl} />
+              <p className="para">{username}</p>
+              <p className="para">{companyName}</p>
+            </div>
+            <button className="but" data-testid="rightArrow" type="button">
+              <img
+                alt="right arrow"
+                src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+                onClick={this.rightArrow}
+              />
+            </button>
+          </div>
+          <p className="para">{description}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
